aa93d9340479eb3f91d309ef0afc9d4193ac191d
Jitanshu <jitanshu.dev27@gmail.com>
2026-06-21 15:31:26 +0530

    add mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const connectDB = require("./config/db");
+
 dotenv.config();
+
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
