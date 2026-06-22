1bc516a54f0c813bbace9be3fa41e7f0bd848844
Jitanshu <jitanshu.dev27@gmail.com>
2026-06-23 00:25:39 +0530

    add user model with password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,19 @@
-const getUsers = (req, res) => {
-  res.json({
-    success: true,
-    message: "User controller working",
-  });
+const User = require("../models/User");
+
+const getUsers = async (req, res) => {
+  try {
+    const user = await User.create({
+      name: "Jitanshu",
+      email: "jitanshu@example.com",
+      password: "123456",
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
