d25ef8aa71e35386b4ccccdc63a69b0866a6544e
Jitanshu <jitanshu.dev27@gmail.com>
2026-06-23 21:21:52 +0530

    add login api with jwt authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,12 @@
 const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
 
 
 const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-
+    
     if (!name || !email || !password) {
       return res.status(400).json({
         success: false,
@@ -13,7 +14,7 @@ const registerUser = async (req, res) => {
       });
     }
 
-
+    
     const userExists = await User.findOne({ email });
 
     if (userExists) {
@@ -23,17 +24,17 @@ const registerUser = async (req, res) => {
       });
     }
 
-  
+    
     const user = await User.create({
       name,
       email,
       password,
     });
 
-
     res.status(201).json({
       success: true,
       message: "User registered successfully",
+      token: generateToken(user._id),
       user: {
         id: user._id,
         name: user.name,
@@ -49,6 +50,42 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+  
+    const user = await User.findOne({ email });
+
+    
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        success: true,
+        message: "Login successful",
+        token: generateToken(user._id),
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+        },
+      });
+    } else {
+      res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   registerUser,
+  loginUser,
 };
